eba682eb278d73e58e1720de9a629ab542f62e13
峰 熊 <xiong257246@outlook.com>
2021-04-08 20:25:24 +0800

    1

---
+++ CSAPP_Lab2/Lab2.3_Parametric_analysis_of_main/Parametric_analysis_of_main.c
@@ -0,0 +1,10 @@
+//
+// Created by Alienware on 2021/4/8.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    printf("hello");
+}
---
+++ CSAPP_Lab_1.1/cmake-build-debug/CMakeFiles/CSAPP_Lab_1_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CSAPP_Lab_1_1.dir/main.c.o"
+  "CSAPP_Lab_1_1"
+  "CSAPP_Lab_1_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CSAPP_Lab_1_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
